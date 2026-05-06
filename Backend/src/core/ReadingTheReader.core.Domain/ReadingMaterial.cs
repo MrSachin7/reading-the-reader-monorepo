1b7e641d0f3cb0fd5cfcfcea7ca6a3cda3835a26
satish627 <satish.grg627@gmail.com>
2026-05-06 12:52:50 +0200

    feat: initialize properties in EyeTrackerDevice and ReadingMaterial classes

--- Backend/src/core/ReadingTheReader.core.Domain/ReadingMaterial.cs
+++ Backend/src/core/ReadingTheReader.core.Domain/ReadingMaterial.cs
@@ -2,13 +2,11 @@
 
 public class ReadingMaterial
 {
-    public Dictionary<int, string> PagedMarkdownText { get; set; }
+    public Dictionary<int, string> PagedMarkdownText { get; set; } = [];
     public int Fontsize { get; set; }
-    public string FontType { get; set; }
+    public string FontType { get; set; } = string.Empty;
     public int LetterSpacing { get; set; }
-    public Dictionary<int, string> MarkdownQuestions { get; set; }
-    
+    public Dictionary<int, string> MarkdownQuestions { get; set; } = [];
 
     public bool CanUserChangeSettingWhileReading { get; set; }
-    
 }
