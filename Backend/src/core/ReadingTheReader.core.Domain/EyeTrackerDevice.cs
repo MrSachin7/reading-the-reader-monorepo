3d6294c429abb3cd0493bad5d839d5d26e7ec53f
satish627 <satish.grg627@gmail.com>
2026-05-06 12:52:50 +0200

    feat: initialize properties in EyeTrackerDevice and ReadingMaterial classes

--- Backend/src/core/ReadingTheReader.core.Domain/EyeTrackerDevice.cs
+++ Backend/src/core/ReadingTheReader.core.Domain/EyeTrackerDevice.cs
@@ -2,9 +2,9 @@ namespace ReadingTheReader.core.Domain;
 
 public class EyeTrackerDevice
 {
-    public string Name { get; set; }
-    public string Model { get; set; }
-    public string SerialNumber { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Model { get; set; } = string.Empty;
+    public string SerialNumber { get; set; } = string.Empty;
     public bool HasSavedLicence { get; set; }
 
     public EyeTrackerDevice Copy()
